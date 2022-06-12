0ecc91328eef3097f6c49de6e4d7e8395ae7a1e4
unknown <erfanhosseinizadeh@gmail.com>
2022-06-12 12:51:53 +0430

    commit message here!!!

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,11 @@ export default function HomePage({ items, categories }) {
    )
 }
 
-export const getStaticProps = async ctx => {
+export const getServerSideProps = async ctx => {
    const raw = await fetch(server + '/api/v1/products')
    const { items, categories, colors } = await raw.json()
 
    return {
       props: { items, categories, colors },
-      revalidate: 10,
    }
 }
