b74f856465da170ad1a1a74974348334546b309a
unknown <erfanhosseinizadeh@gmail.com>
2022-06-12 12:57:11 +0430

    commit message here!!!

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000'
+export const server = dev ? 'http://localhost:3000' : process.env.PRODUCTION_URL
 
 export const mongoUrl =
    'mongodb+srv://vsCode:erfan123@mongocluster.htzk2.mongodb.net/e-commerce?retryWrites=true&w=majority'
